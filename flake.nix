d4329516c50208697826b54269810d2505f43a44
Sönke Hahn <soenkehahn@gmail.com>
2024-03-14 15:55:27 -0700

    Add hello world program

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.cargo2nix = {
+    url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-compat.follows = "flake-compat";
+    inputs.flake-utils.follows = "flake-utils";
+  };
+  outputs = { nixpkgs, flake-utils, cargo2nix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ cargo2nix.overlays.default ];
+        };
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.75.0";
+          packageFun = import ./Cargo.nix;
+          extraRustComponents = [ "clippy" ];
+        };
+      in
+      {
+        packages.default = rustPkgs.workspace.comment { };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.rust-analyzer ];
+        };
+        apps.generateCargoNix = {
+          type = "app";
+          program =
+            pkgs.lib.getExe (pkgs.writeShellApplication
+              {
+                name = "generateCargoNix";
+                runtimeInputs = [ cargo2nix.packages.${system}.default ];
+                text = ''
+                  cargo2nix . --overwrite --locked
+                '';
+              });
+        };
+      }
+    );
+}
