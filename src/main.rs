9e50d2476d6b0e6371e93b7908336ad475e2ad9f
Alex David <git@alexd.me>
2024-06-10 12:07:00 -0700

    Forward child exit codes

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,59 @@
-use anyhow::{Context, Result};
-use std::env;
-use std::process::exit;
-use std::process::Command;
+use std::{
+    env, io,
+    os::unix::process::ExitStatusExt,
+    process::{self, Command},
+};
 
 fn main() {
-    match run(env::args()) {
-        Ok(()) => {}
-        Err(err) => {
-            eprintln!("{}", err);
-            exit(1);
-        }
+    let Exit { code, msg } = run(env::args())
+        .err()
+        .unwrap_or(Exit { code: 0, msg: None });
+    if let Some(msg) = msg {
+        eprintln!("{msg}");
     }
+    process::exit(code);
 }
 
-fn run(mut args: impl Iterator<Item = String>) -> Result<()> {
+fn run(mut args: impl Iterator<Item = String>) -> Result<(), Exit> {
     while let Some(arg) = args.next() {
         if arg == "--" {
             break;
         }
     }
-    let command = args.next().context("comment: no command given")?;
+    let command = args
+        .next()
+        .ok_or(Exit::error("comment: no command given"))?;
     let command_args: Vec<String> = args.collect();
-    Command::new(command).args(command_args).spawn()?.wait()?;
-    Ok(())
+    let exit = Command::new(&command).args(command_args).spawn()?.wait()?;
+    match exit.code() {
+        Some(0) => Ok(()),
+        Some(code) => Err(Exit { code, msg: None }),
+        None => {
+            let signal = exit.signal().unwrap_or(0);
+            Err(Exit {
+                code: 128 + signal,
+                msg: Some(format!("{command} terminated by signal {signal}")),
+            })
+        }
+    }
+}
+
+struct Exit {
+    code: i32,
+    msg: Option<String>,
+}
+
+impl Exit {
+    fn error(msg: impl ToString) -> Self {
+        Self {
+            code: 1,
+            msg: Some(msg.to_string()),
+        }
+    }
+}
+
+impl From<io::Error> for Exit {
+    fn from(err: io::Error) -> Self {
+        Exit::error(format!("{err}"))
+    }
 }
--- tests/executable.rs
+++ tests/executable.rs
@@ -1,5 +1,5 @@
 use assert_cmd::Command;
-use std::iter::empty;
+use std::{ffi::OsStr, iter::empty, thread};
 use tempfile::tempdir;
 
 #[test]
@@ -44,6 +44,51 @@ fn allows_comments() {
     assert!(dir.path().join("file").exists());
 }
 
+#[test]
+fn exits_with_child_exit_code() {
+    run()
+        .args(["--", "bash", "-c", "exit 42"])
+        .assert()
+        .failure()
+        .code(42);
+}
+
+#[test]
+fn exits_with_128_plus_signal_if_child_is_killed() {
+    for signal in [
+        /* SIGHUP */ 1, /* SIGINT */ 2, /* SIGQUIT */ 3, /* SIGKILL */ 9,
+        /* SIGTERM */ 15,
+    ] {
+        let t = thread::spawn(move || {
+            run()
+                .args(["comment-test-kill-child", "--", "sleep", "inf"])
+                .assert()
+                .failure()
+                .stderr(format!("sleep terminated by signal {signal}\n"))
+                .code(128 + signal)
+        });
+        let comment_pid = pgrep(&["--full", "comment-test-kill-child"]);
+        let sleep_pid = pgrep(&["--parent", &format!("{comment_pid}")]);
+        Command::new("kill")
+            .args([format!("-{signal}"), format!("{sleep_pid}")])
+            .assert()
+            .success();
+        t.join().unwrap();
+    }
+}
+
 fn run() -> Command {
     Command::cargo_bin("comment").unwrap()
+}
+
+fn pgrep(pgrep_args: &[impl AsRef<OsStr>]) -> u32 {
+    // Keep attempting command until it succeeds in case process hasn't started yet
+    let stdout = loop {
+        let output = Command::new("pgrep").args(pgrep_args).output().unwrap();
+        if output.status.success() {
+            break output.stdout;
+        }
+        thread::sleep(std::time::Duration::from_millis(200));
+    };
+    String::from_utf8(stdout).unwrap().trim().parse().unwrap()
 }
